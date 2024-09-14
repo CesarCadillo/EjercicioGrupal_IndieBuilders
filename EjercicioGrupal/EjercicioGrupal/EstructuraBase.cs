6842ca54177b40f7888329dc5ecc6fa152e33627
RaphaelXZA <108958428+RaphaelXZA@users.noreply.github.com>
2024-09-13 22:58:59 -0500

    Clases de Estructuras

---
+++ EjercicioGrupal/EjercicioGrupal/EstructuraBase.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EjercicioGrupal
+{
+    internal abstract class EstructuraBase
+    {
+        private string nombre;
+        private int precio;
+        private int vida;
+
+        protected EstructuraBase(string nombre, int precio, int vida)
+        {
+            this.nombre = nombre;
+            this.precio = precio;
+            this.vida = vida;
+        }
+
+        public abstract void HabilidadDeLaEstructura(int valor);
+
+    }
+}
---
+++ EjercicioGrupal/EjercicioGrupal/EstructuraRecolectora.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EjercicioGrupal
+{
+    internal class EstructuraRecolectora : EstructuraBase
+    {
+        private int dineroQueRecolecta;
+        
+        public EstructuraRecolectora(string nombre, int precio, int vida, int dineroQueRecolecta) : base(nombre, precio, vida)
+        {
+            this.dineroQueRecolecta = dineroQueRecolecta;
+        }
+
+        public override void HabilidadDeLaEstructura(int valor)
+        {
+            valor += dineroQueRecolecta;
+        }
+    }
+}
